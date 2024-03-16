0f5389a7acdfefa78ec62eee2e4c40f200aa313c
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-16 12:56:27 +0300

    Theme view.

---
+++ Scrumdinger/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Ekomobong Edeme on 16/03/2024.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme:Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(theme.mainColor)
+            .foregroundStyle(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
