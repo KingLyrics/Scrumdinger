30eaa73d0fc934ee65401267ce81fcc3e4c8e779
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-16 10:37:09 +0300

    Screen changes.

--- Scrumdinger/ContentView.swift
+++ Scrumdinger/ContentView.swift
@@ -12,16 +12,30 @@ struct ContentView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment:.leading) {
                     Text("Seconds Elapsed")
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .font(.subheadline)
+            
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "forward.fill")
+                })
+            }
         }
+        .padding()
     }
 }
 
