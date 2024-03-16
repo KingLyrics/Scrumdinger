3b0e03cfa94c77215f67bfc9fdd7f1a3cd29c725
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-16 11:01:41 +0300

    CardView created and completed.

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Ekomobong Edeme on 16/03/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum:DailyScrumModel
+    var body: some View {
+        VStack(alignment:.leading) {
+            Text(scrum.title)
+                .font(.headline)
+          Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+    }
+        
+}
+
+#Preview {
+    CardView(scrum: MockData().sampleData[1])
+        .background(MockData().sampleData[1].theme.mainColor)
+}
