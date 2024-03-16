81c8f527b96144402bb73496ad2b5994c62d19c9
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-16 11:22:44 +0300

    Data in a list.

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -10,22 +10,24 @@ import SwiftUI
 struct CardView: View {
     let scrum:DailyScrumModel
     var body: some View {
-        VStack(alignment:.leading) {
-            Text(scrum.title)
-                .font(.headline)
-          Spacer()
-            HStack{
-                Label("\(scrum.attendees.count)", systemImage: "person.3")
-                Spacer()
-                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
-                    .labelStyle(.trailingIcon)
-            }
-            .font(.subheadline)
-        }
-        .padding()
-        .foregroundStyle(scrum.theme.accentColor)
-    }
+        VStack(alignment: .leading) {
+                   Text(scrum.title)
+                       .font(.headline)
+                   Spacer()
+                   HStack {
+                       Label("\(scrum.attendees.count)", systemImage: "person.3")
+                       Spacer()
+                       Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                           .labelStyle(.trailingIcon)
+                   }
+                   .font(.caption)
+                  
+               }
+               .padding()
+               .frame(width: .infinity)
+               .foregroundColor(scrum.theme.accentColor)
+               
+           }
         
 }
 
---
+++ Scrumdinger/ScrumList.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumList.swift
+//  Scrumdinger
+//
+//  Created by Ekomobong Edeme on 16/03/2024.
+//
+
+import SwiftUI
+
+struct ScrumList: View {
+    let scrums:[DailyScrumModel]
+    var body: some View {
+        List(scrums, id:\.title){ scrum in
+            CardView(scrum: scrum)
+                .background(scrum.theme.mainColor)
+        }
+        
+    }
+}
+
+#Preview {
+    ScrumList(scrums: MockData().sampleData)
+}
