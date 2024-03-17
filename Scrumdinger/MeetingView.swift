d783237f4b906a9a5a7eb52efef891ec594567e6
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-17 10:55:02 +0300

    Meeting View Updates.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,36 +8,46 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum:DailyScrumModel
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment:.leading) {
-                    Text("Seconds Elapsed")
-                    Label("300", systemImage: "hourglass.tophalf.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                ProgressView(value: 5, total: 15)
+                HStack {
+                    VStack(alignment:.leading) {
+                        Text("Seconds Elapsed")
+                        Label("300", systemImage: "hourglass.tophalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("Seconds Remaining")
+                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+                    }
                 }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                .font(.subheadline)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                
+                HStack{
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "forward.fill")
+                    })
                 }
             }
-            .font(.subheadline)
-            Circle()
-                .strokeBorder(lineWidth: 24)
+            .padding()
             
-            HStack{
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "forward.fill")
-                })
-            }
         }
         .padding()
+        .foregroundStyle(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    MeetingView()
+    MeetingView(scrum: .constant(MockData().sampleData[0]))
 }
