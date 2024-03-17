70133d51ff5457ec28dcb7bbec4696cf8bafa9f7
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-17 11:40:45 +0300

    Scrum Meeting View.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct MeetingView: View {
     @Binding var scrum:DailyScrumModel
+    @StateObject var scrumTimer = ScrumTimer()
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
-               
+                
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
+                
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 
@@ -32,6 +37,13 @@ struct MeetingView: View {
         }
         .padding()
         .foregroundStyle(scrum.theme.accentColor)
+        .onAppear{
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            
+        }
+        .onDisappear {
+                    scrumTimer.stopScrum()
+                }
         .navigationBarTitleDisplayMode(.inline)
     }
 }
